388a86de5bfd34700da5fd580f5481f355f5165d
juandaxx <juan.goez@upb.edu.co>
2019-10-14 17:08:16 -0500

    Feed cursos agregado
    Se agregó el feed de cursos y la funcionalidad de este.

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/*
+This method change the section and has 2 parameters:
+    pageOut: The section ID that you go out
+    pageIn: The section ID that you go in 
+*/
+function page(pageOut1, pageOut2, pageIn) {
+    if(document.getElementById(pageOut1).className != "ocultar"){
+        document.getElementById(pageOut1).className += " ocultar";
+    }
+    if(document.getElementById(pageOut2).className != "ocultar"){
+        document.getElementById(pageOut2).className += " ocultar";
+    }
+    
+    document.getElementById(pageIn).classList.remove("ocultar");
+    window.scroll(0, document.getElementById(pageIn).scrollTop);
+}
+
+function pageVideos(pageOut1,pageOut2,pageOut3, pageIn) {
+    if(document.getElementById(pageOut1).className != "ocultar"){
+        document.getElementById(pageOut1).className += " ocultar";
+    }
+    if(document.getElementById(pageOut2).className != "ocultar"){
+        document.getElementById(pageOut2).className += " ocultar";
+    }
+    if(document.getElementById(pageOut3).className != "ocultar"){
+        document.getElementById(pageOut3).className += " ocultar";
+    }
+
+    document.getElementById(pageIn).classList.remove("ocultar");
+    window.scroll(0, document.getElementById(pageIn).scrollTop);
+}
