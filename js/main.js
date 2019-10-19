e4c273ba4d73184eae90a067b7d9ecbf282e59dc
axew26 <33433481+axew26@users.noreply.github.com>
2019-10-19 14:39:50 -0500

    Creacion Seccion de creditos
    Se creó en el html la seccion de los creditos y en el js la posibilidad de pasar a ella como se hace con las demas secciones

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ This method change the section and has 2 parameters:
     pageOut: The section ID that you go out
     pageIn: The section ID that you go in 
 */
-function page(pageOut1, pageOut2, pageIn) {
+function page(pageOut1, pageOut2, pageOut3, pageIn) {
     if(document.getElementById(pageOut1).className != "ocultar"){
         document.getElementById(pageOut1).className += " ocultar";
     }
     if(document.getElementById(pageOut2).className != "ocultar"){
         document.getElementById(pageOut2).className += " ocultar";
+    }
+    if(document.getElementById(pageOut3).className != "ocultar"){
+        document.getElementById(pageOut3).className += " ocultar";
     }
     //cambio de color del fondo al moverse entre vistas, eliminar este codigo despues de las evidencias
     if(pageIn == "cursos"){
@@ -20,6 +23,9 @@ function page(pageOut1, pageOut2, pageIn) {
     }
     if(pageIn == "juego"){
         document.getElementById("div_container").style.backgroundColor = "red";
+    }
+    if(pageIn == "creditos"){
+        document.getElementById("div_container").style.backgroundColor = "blue";
     }
     //Eliminar hasta acá
     document.getElementById(pageIn).classList.remove("ocultar");
