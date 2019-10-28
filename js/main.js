7c707bbac16afb63eafaf7ab280abdd10ff37de5
juandaxx <juan.goez@upb.edu.co>
2019-10-28 15:25:00 -0500

    Feed cursos: nagevación funcional
    El feed de cursos está completamente funcional.

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function page(pageOut1, pageOut2, pageOut3, pageIn) {
     window.scroll(0, document.getElementById(pageIn).scrollTop);
 }
 
-function pageVideos(pageOut1,pageOut2,pageOut3, pageIn) {
+function navegacionCursos(pageOut1,pageOut2,pageOut3,pageOut4,pageOut5, pageIn) {
     if(document.getElementById(pageOut1).className != "ocultar"){
         document.getElementById(pageOut1).className += " ocultar";
     }
@@ -27,6 +27,12 @@ function pageVideos(pageOut1,pageOut2,pageOut3, pageIn) {
     }
     if(document.getElementById(pageOut3).className != "ocultar"){
         document.getElementById(pageOut3).className += " ocultar";
+    }
+    if(document.getElementById(pageOut4).className != "ocultar"){
+        document.getElementById(pageOut4).className += " ocultar";
+    }
+    if(document.getElementById(pageOut5).className != "ocultar"){
+        document.getElementById(pageOut5).className += " ocultar";
     }
 
     document.getElementById(pageIn).classList.remove("ocultar");
