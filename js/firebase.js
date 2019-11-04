33643280f87ef3e76088d1c86970b81a060a72d0
juandaxx <juan.goez@upb.edu.co>
2019-11-03 20:38:46 -0500

    login, registro y reestablecer contraseña agregados
    Se realizó la interfaz visual del login, el registo y la sección de reestablecer contraseña

---
+++ js/firebase.js
@@ -0,0 +1,110 @@
+function registrar() {
+    var emailRegistro = document.getElementById('emailRegistro').value;
+    var passwordRegistro = document.getElementById('passwordRegistro').value;
+    var password2Registro = document.getElementById('password2Registro').value;
+
+    if (passwordRegistro == password2Registro) {
+        firebase.auth().createUserWithEmailAndPassword(emailRegistro, passwordRegistro)
+            .then(function () {
+                verificar();
+            })
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode);
+                console.log(errorMessage);
+            });
+    } else {
+        document.getElementById("contrasenasDiferentes").innerHTML = "¡Las contraseñas deben coincidir!";
+    }
+}
+
+function ingreso() {
+    var emailLogin = document.getElementById('emailLogin').value;
+    var passwordLogin = document.getElementById('passwordLogin').value;
+
+    firebase.auth().signInWithEmailAndPassword(emailLogin, passwordLogin)
+    .then(function () {
+        observador();
+    })
+    .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
+}
+
+function observador() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+
+            if (user.emailVerified) {
+                iniciarCerrarSesion('inicioWawa', 'contenidoWawa');
+                mostrarBotonLogOut();
+            } 
+        } else {
+            console.log('No existe usuario activo');
+            // User is signed out.
+            // ...
+        }
+    });
+}
+
+observador();
+
+function mostrarBotonLogOut() {
+    document.getElementById('botonLogOut').innerHTML = `
+    <div class="mdl-grid">
+        <button
+            class="mdl-cell mdl-cell--12-col mdl-button mdl-button--raised mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
+            onclick="cerrar();">
+            Cerrar sesión
+        </button>
+    </div>`;
+}
+
+function cerrar() {
+    firebase.auth().signOut()
+        .then(function () {
+            iniciarCerrarSesion('contenidoWawa', 'inicioWawa');
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+function verificar() {
+    var user = firebase.auth().currentUser;
+
+    user.sendEmailVerification().then(function () {
+        // Email sent.
+        alert('Se ha enviado un correo de verificación');
+        inicioWawa('recuperarWawa', 'registroWawa', 'loginWawa');
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+function recuperarContrasena() {
+    var auth = firebase.auth();
+    var emailRecuperar = document.getElementById('emailRecuperar').value;
+
+    auth.sendPasswordResetEmail(emailRecuperar).then(function () {
+        // Email sent.
+        alert('Se ha enviado un correo de verificación');
+        inicioWawa('recuperarWawa', 'registroWawa', 'loginWawa');
+    }).catch(function (error) {
+        // An error happened.
+    });
+}
